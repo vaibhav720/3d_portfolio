4f1b48ac128727ac700fa59c8a5a59739cbed527
Vaibhav Dipesh Parikh <parikhvaibhav19@gmail.com>
2024-07-22 11:31:46 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-
-
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 const App = ()=> {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      3d developer portfolio
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar/>
+        <Hero/>
+
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+
     </div>
+    </BrowserRouter>
   )
 }
 
